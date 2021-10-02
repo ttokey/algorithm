bef78eba72de4ac08f1ca865235ad8f8be491246
ttokey <79037443+ttokey@users.noreply.github.com>
2021-10-02 15:17:28 +0900

    feat : devmatching 풀어보는 중

--- src/main/java/programmers/devMatching/Lotto.java
+++ src/main/java/programmers/devMatching/Lotto.java
@@ -1,2 +1,46 @@
-package programmers.devMatching;public class Lotto {
+package programmers.devMatching;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int countMatching = countMatching(toList(lottos), toList(win_nums));
+        int countZero = countZero(toList(lottos));
+        int[] answer = {7 - countMatching - countZero, 7 - countMatching};
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == 7) {
+                answer[i] = 6;
+            }
+        }
+        return answer;
+    }
+
+    public int countMatching(List<Integer> lottos, List<Integer> win_nums) {
+        int count = 0;
+        for (int i : lottos) {
+            if (i != 0 && win_nums.contains(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countZero(List<Integer> lottos) {
+        int count = 0;
+        for (int i : lottos) {
+            if (i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public List<Integer> toList(int[] input) {
+        return Arrays.stream(input)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/programmers/devMatching/RotatingEdge.java
+++ src/main/java/programmers/devMatching/RotatingEdge.java
@@ -1,2 +1,54 @@
-package programmers.devMatching;public class RotatingEdge {
+package programmers.devMatching;
+
+//https://programmers.co.kr/learn/courses/30/lessons/77485
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotatingEdge {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public class RotatingMap {
+        int[][] nodes;
+
+        public RotatingMap(int rows, int columns) {
+            nodes = new int[rows][columns];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    this.nodes[i][j] = i * columns + j + 1;
+                }
+            }
+        }
+
+        public int rotate(int x1, int y1, int x2, int y2) {
+            // 우 하 좌 상
+            int[][] moves = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+            List<Integer> numbers = new ArrayList<>();
+            for (int i = x1 - 1; i < x2 - 1; i++) {
+                numbers.add(nodes[i][y1 - 1]);
+            }
+            for (int j = y1 - 1; j < y2 - 1; j++) {
+                numbers.add(nodes[x2 - 1][j]);
+            }
+            for (int i = x2 - 1; i >= x1 - 1; i--) {
+
+            }
+
+        }
+
+    }
+
+    public class Node {
+        int x;
+        int y;
+        int value;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
