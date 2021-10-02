6d0996e435931b263e415c455c3adbf8136f4df6
ttokey <ttokey.lee@gmail.com>
2021-10-02 22:48:02 +0900

    feat : 위클리 챌린지 8주차

---
+++ src/main/java/programmers/weeklychallenge/Eight.java
@@ -0,0 +1,23 @@
+package programmers.weeklychallenge;
+
+public class Eight {
+    class Solution {
+        public int solution(int[][] sizes) {
+            int firstMax = 0;
+            int secondMax = 0;
+
+            for (int[] size : sizes) {
+                if (size[0] > size[1]) {
+                    firstMax = Math.max(firstMax, size[0]);
+                    secondMax = Math.max(secondMax, size[1]);
+                } else {
+                    firstMax = Math.max(firstMax, size[1]);
+                    secondMax = Math.max(secondMax, size[0]);
+                }
+            }
+
+            int answer = firstMax * secondMax;
+            return answer;
+        }
+    }
+}
