cf1602a251810d7ce8c413e645c0aa5d5d5427fb
이석희 <seokhee.lee2@cj.net>
2022-11-10 15:44:03 +0900

    feat : programmers

---
+++ src/main/java/programmers/practice/Boxes_131704.java
@@ -0,0 +1,25 @@
+package programmers.practice;
+
+import java.util.Stack;
+
+public class Boxes_131704 {
+    public int solution(int[] order) {
+        Stack<Integer> tempBelt = new Stack<>();
+        int orderIndex = 0;
+        int count = 0;
+        for (int box = 1; box <= order.length; box++) {
+            if (box == order[orderIndex]) {
+                count++;
+                orderIndex++;
+            } else {
+                tempBelt.push(box);
+            }
+            while (!tempBelt.isEmpty() && tempBelt.peek() == order[orderIndex]) {
+                count++;
+                orderIndex++;
+                tempBelt.pop();
+            }
+        }
+        return count;
+    }
+}
