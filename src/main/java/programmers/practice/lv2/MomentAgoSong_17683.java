211c9a06da969876cb662c10d5361b1bdda68b79
ttokey <ttokey.lee@gmail.com>
2023-01-24 23:51:15 +0900

    programmers

---
+++ src/main/java/programmers/practice/lv2/MomentAgoSong_17683.java
@@ -0,0 +1,14 @@
+package programmers.practice.lv2;
+
+import java.time.LocalDate;
+
+public class MomentAgoSong_17683 {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "";
+        return answer;
+    }
+
+    public class MusicInfo {
+        LocalDate start;
+    }
+}
